c5a5fe5605ef92a1cbf3d0c5a501523812e81d98
Patrick McDaniel <dfwjpm@outlook.com>
2022-12-13 13:23:23 -0600

    add prop forwarding test case

--- App/components/__tests__/Form.test.js
+++ App/components/__tests__/Form.test.js
@@ -9,3 +9,22 @@ test("renders the passed label", () => {
   expect(getByText("Test Label")).not.toBeNull();
   expect(queryByText("ASDF")).toBeNull();
 });
+
+// testing props to text input
+test("forwards remaining props to the underlying TextInput", () => {
+  const { getByTestId } = render(
+    <TextField label="Test Label" passedProp="yes" />
+  );
+
+  // see the output from the props
+  console.log(
+    'getByTestId("Form.TextInput").props: ',
+    getByTestId("Form.TextInput").props
+  );
+
+  expect(getByTestId("Form.TextInput").props).toEqual(
+    expect.objectContaining({
+      passedProp: "yes",
+    })
+  );
+});
